736ecb039cd095ed9fe1b26ef8f515e7f6081e6d
BerniceKUO <bernicekuo0419@gmail.com>
2020-04-15 08:36:03 +0800

    Update media.py

--- media.py
+++ media.py
@@ -1,6 +1,7 @@
 import webbrowser
 class Movie():
-    VALID_RATINGS=["G","PG","PG-13","R"]
+    #這個是電影的共用類別   
+    VALID_RATINGS=["普遍級","保護級","輔導級","限制級"]#不會改變的常數變數名稱要用大寫
     def __init__(self,movie_title,movie_storyline,poster_image,trailer_youtube_url):
         self.title=movie_title
         self.storyline=movie_storyline
@@ -9,3 +10,11 @@ class Movie():
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
+class TVShow():
+     #這個是影集的共用類別
+     def __init__(self,tv_season,tv_episode,tv_station,trailer_youtube_url):
+        self.season=tv_season
+        self.episode=tv_episode
+        self.station=tv_station
+        self.trailer_youtube_url=trailer_youtube_url
+    
