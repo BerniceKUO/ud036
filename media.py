f51b7bdcf5d68bb6b4138bb61ced43f80866202e
BerniceKUO <37724364+BerniceKUO@users.noreply.github.com>
2020-04-16 22:35:19 +0800

    Update media.py

--- media.py
+++ media.py
@@ -1,20 +1,28 @@
 import webbrowser
-class Movie():
+import mediaSuper
+class Movie(mediaSuper.Video):
     #這個是電影的共用類別   
     VALID_RATINGS=["普遍級","保護級","輔導級","限制級"]#不會改變的常數變數名稱要用大寫
-    def __init__(self,movie_title,movie_storyline,poster_image,trailer_youtube_url):
-        self.title=movie_title
-        self.storyline=movie_storyline
-        self.poster_image_url=poster_image
-        self.trailer_youtube_url=trailer_youtube_url
+    def __init__(self,movie_title,movie_storyline,poster_image,trailer_youtube_url,duration,movie_ratings):
+        mediaSuper.Video.__init__(self,movie_title,movie_storyline,poster_image,trailer_youtube_url,duration)
+        self.ratings=movie_ratings;
 
-    def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
-class TVShow():
+    #def show_trailer(self):
+        #webbrowser.open(self.trailer_youtube_url)
+    def show_ratings(self):
+        return self.VALID_RATINGS[self.ratings]
+
+class TVShow(mediaSuper.Video):
      #這個是影集的共用類別
-     def __init__(self,tv_season,tv_episode,tv_station,trailer_youtube_url):
+     def __init__(self,movie_title,movie_storyline,poster_image,trailer_youtube_url,duration,tv_class,tv_season,tv_episode):
+        mediaSuper.Video.__init__(self,movie_title,movie_storyline,poster_image,trailer_youtube_url,duration)
         self.season=tv_season
         self.episode=tv_episode
-        self.station=tv_station
         self.trailer_youtube_url=trailer_youtube_url
-    
+        self.tvclass=tv_class
+     def show_trailer(self):
+         print("===test===")
+         webbrowser.open_new_tab(self.trailer_youtube_url)
+     def show_ratings(self):
+         return self.tvclass
+
